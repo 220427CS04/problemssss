5e9a7d43bb1614f3e9edb3d616854b105b1e4c4f
Mirkamol Mirazimov <113422166+220427CS04@users.noreply.github.com>
2022-11-09 23:21:41 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,71 +1,109 @@
-//#include <iostream>
-//using namespace std;
-//
-//
-//int main(){
-//
-//    int num;
-//    cout<<"Enter a number: ";
-//    cin>>num;
-//
-//    if(num%3==0){
-//        cout<<"YES \n";
-//    } else
-//        cout<<"NO \n";
-//
-//
-//}
-
-//#include <iostream>
-//using namespace std;
-//
-//int get_max(int row[], int size){
-//    int max=row[0];
-//    for(int i=0; i<size; i++){
-//        if(max<row[i]){
-//            max=row[i];
-//        }
-//
-//    }
-//
-//    return max;
-//}
-//
-//int main(){
-//
-//    int row[5]={213, 321, 56, 789, 3};
-//    int size= sizeof(row)/sizeof(row[0]);
-//
-//    cout<<get_max(row, size)<<endl;
-//
-//}
-
-
 #include <iostream>
+
 using namespace std;
 
-bool isPrime(int n) {
-    if (n <= 1)
-        return false;
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-    return true;
+
+//Problem 1
+
+
+/*
+void average(int size, int array[]){
+int sum=0;
+double aver;
+    for(int i=0; i<size; i++){
+cin>>array[i];
+        sum+=array[i];
+    }
+aver=static_cast <double>(sum)/size;
+    cout<<"sum is: "<<sum<<endl<<"average is: "<<aver<<endl;
 }
 
-int main (){
-    int num, counter = 0, i = 0;
-    cout<<"Enter a number: ";
-    cin>>num;
-    while(true) {
-        if (isPrime(i)) {
-            cout << " " << i;
-            counter++;
-        }
-        if (counter == num) {
-            break;
-        }
-        i++;
-    }
-    cout<<endl;
+int main() {
+
+    int size;
+    cin >> size;
+    int array[size];
+
+
+    average(size, array);
+    cout << endl;
 }
+*/
+
+
+//Problem 2
+/*
+
+
+void gpa(double grade[]){
+
+    double sum=0;
+    double average;
+    for(int i=0; i<4; i++){
+        sum+=grade[i];
+    }
+    average=sum/4;
+    cout<<average;
+
+
+}
+
+int main(){
+    string subjects[]={"English", "ICT", "Computer Programming", "Math"};
+double array[4];
+for(int i=0; i<4; i++){
+cin>>array[i];
+}
+for(int i=0; i<4; i++){
+    cout<<subjects[i]<<": "<<array[i]<<endl;
+}
+    gpa(array);
+}
+*/
+
+
+//Problem3
+
+
+//void min_number( int size){
+//    double array[size];
+//    for(int i=0; i<size; i++){
+//        cin>>array[i];
+//    }
+//    double min=array[0];
+//    for(int i=0; i<size;i++){
+//        if(min>array[i]){
+//            min=array[i];
+//        }
+//    }
+//cout<<"Min is: "<<min<<endl;
+//}
+//
+//int main(){
+//    int size;
+//    cin>>size;
+//
+//    min_number(size);
+//
+//
+//
+//}
+
+//problem 4
+
+/*
+int x=220427;
+
+int fun(){
+    return ::x+1000;
+}
+
+int main(){
+    int x=2005;
+    cout<<::x<<" is id"<<endl;
+    cout<<x<<" is a birth year"<<endl;
+    cout<<"Product of these numbers is: "<<::x*x<<endl;
+cout<<"Calling function: "<<fun();
+}*/
+
+
