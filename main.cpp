d5c986f648b9d2367bf6f4ec7831bae4bedbb13f
Mirkamol Mirazimov <113422166+220427CS04@users.noreply.github.com>
2022-11-19 01:00:35 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+//problem 10
+//int main(){
+//
+//    int size;
+//    cin>>size;
+//    int arr[size];
+//    for (int i=0; i<size; i++){
+//        cin>>arr[i];
+//    }
+//    cout<<"Original array: ";
+//    for(int i=0; i<size; i++){
+//        cout<<arr[i]<<" ";
+//    }
+//    cout<<endl;
+//    cout<<"Reversed array: ";
+//    for(int i=size-1; i>=0; i--){
+//        cout<<arr[i]<<" ";
+//    }
+//    cout<<endl;
+//}
+
+
+//problem 11
+//void reverse(int arr[], int size){
+//    if(size>=1) {
+//        cout << arr[size - 1] << " ";
+//        reverse(arr, size - 1);
+//
+//    }
+//}
+//
+//int main (){
+//    int size;
+//    cin>>size;
+//    int array[size];
+//    for(int i=0; i<size; i++){
+//        cin>>array[i];
+//    }
+//    reverse(array, size);
+//
+//}
+//problem 12
+//int main(){
+//    int size;
+//    cin>>size;
+//    int arr[size];
+//
+//
+//    for(int i=0; i<size; i++){
+//        cin>>arr[i];
+//    }
+//    cout<<"Original array: ";
+//    for(int i=0; i<size; i++){
+//        cout<<arr[i]<<" ";
+//    }
+//    cout<<endl;
+//
+//    for(int i=0; i<size; i++){
+//        for(int l=1; l<size; l++){
+//            if(arr[l]<arr[l-1]) {
+//                int temp = arr[l];
+//                arr[l] = arr[l - 1];
+//                arr[l - 1] = temp;
+//            }
+//        }
+//    }
+//    cout<<"Sorted array: ";
+//    for(int i=0; i<size; i++){
+//        cout<<arr[i]<<" ";
+//    }
+//    cout<<endl;
+//}
+//
+
+//problem 13
+
+void occur(int arr[], int size, int x){
+    int counter=0;
+    for(int i=0; i<size; i++){
+        if(arr[i]==x){
+            counter++;
+        }
+    }
+    cout<<counter;
+}
+
+int main(){
+    int arrr[ ] = {1, 1, 2, 2, 2, 2, 3};
+    int x = 2;
+   int size=sizeof(arrr)/sizeof(arrr[0]);
+ occur(arrr, size, x);
+}
